f8bd6629481d74660d3e73ca9e08e6151bfe0eed
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-01-28 01:40:50 -0600

    Implement refactored code

--- ml_dl_playground/iris/main.py
+++ ml_dl_playground/iris/main.py
@@ -1,24 +1,47 @@
 from pathlib import Path
-from typing import Tuple
+from typing import Any, List, Tuple
 from warnings import filterwarnings
 
 from numpy import ndarray
-from prepareData import prepare
-from train import trainIntelSVC, trainSVC
+from progress.bar import Bar
+from sklearn.svm import SVC
+from sklearnex.svm import SVC as intelSVC
+
+from ml_dl_playground.iris import train
+from ml_dl_playground.iris.prepareData import load
+from ml_dl_playground.utils import fs
 
 filterwarnings(action="ignore")
 
 
 def main() -> None:
-    filepath: Path = Path("../../data/iris/bezdekIris.data")
+    dataFilepath: Path = Path("../../data/iris/bezdekIris.data")
+    absoluteDataFilepath: Path = fs.convertRelativePathToAbsolute(path=dataFilepath)
 
-    splits: Tuple[ndarray, ndarray, ndarray, ndarray] = prepare(
-        filepath=filepath,
-        testSize=0.3,
-    )
+    data: Tuple[
+        List[Tuple[ndarray, ndarray, ndarray, ndarray]],
+        Tuple[ndarray, ndarray],
+    ] = load(filepath=absoluteDataFilepath)
 
-    trainIntelSVC(data=splits)
-    trainSVC(data=splits)
+    trainValidationSplits: List[Tuple[ndarray, ndarray, ndarray, ndarray]] = data[0]
+    testSplits: Tuple[ndarray, ndarray] = data[1]
+
+    gridSearch: List[dict[str, Any]] = train.DEFAULT_SVC_PARAMETERS
+    gsSize: int = len(gridSearch)
+
+    with Bar("Training SVC models...", max=gsSize) as bar:
+        parameters: dict[str, Any]
+        for parameters in gridSearch:
+            model: SVC = SVC(**parameters)
+
+            datum: Tuple[ndarray, ndarray, ndarray, ndarray]
+            for datum in trainValidationSplits:
+                xTrain: ndarray = datum[0]
+                yTrain: ndarray = datum[2]
+
+                model.fit(X=xTrain, y=yTrain)
+
+            bar.next()
 
 
 if __name__ == "__main__":
