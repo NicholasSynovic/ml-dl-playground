0350d41a2d7e82336aebc1f66d7afa5e7b8beebe
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-01-28 00:46:51 -0600

    Add method to save model as ONNX file

--- ml_dl_playground/utils/fs.py
+++ ml_dl_playground/utils/fs.py
@@ -1,6 +1,13 @@
 from os import mkdir
 from os.path import abspath
 from pathlib import Path
+from typing import Any, Tuple
+
+from numpy import ndarray
+from skl2onnx import to_onnx
+from sklearn.base import BaseEstimator
+
+from ml_dl_playground.utils import fs
 
 
 def convertRelativePathToAbsolute(path: Path) -> Path:
@@ -10,3 +17,16 @@ def convertRelativePathToAbsolute(path: Path) -> Path:
 def createDirectory(path: Path) -> None:
     absolutePath: Path = convertRelativePathToAbsolute(path=path)
     mkdir(path=absolutePath)
+
+
+def saveSKLearnModelToONNX(
+    model: BaseEstimator,
+    outputPath: Path,
+    trainingData: ndarray,
+) -> None:
+    absoluteOutputPath: Path = fs.convertRelativePathToAbsolute(path=outputPath)
+
+    onnxModel: Tuple[Any, Any] = to_onnx(model=model, X=trainingData[:1])
+    with open(absoluteOutputPath, "wb") as onnxFile:
+        onnxFile.write(onnxModel.SerializeToString())
+        onnxFile.close()
