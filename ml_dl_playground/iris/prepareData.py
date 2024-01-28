b9db9444481089ab19cedc396dae678ada50a67c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-01-28 01:10:23 -0600

    Simplify data prep script

--- ml_dl_playground/iris/prepareData.py
+++ ml_dl_playground/iris/prepareData.py
@@ -17,15 +17,61 @@ COLUMNS: List[str] = [
 
 def load(
     filepath: Path,
+    splits: int = 10,
     randomState: int = 42,
-) -> Tuple[ndarray, ndarray, ndarray, ndarray]:
+) -> Tuple[List[Tuple[ndarray, ndarray, ndarray, ndarray]], Tuple[ndarray, ndarray]]:
+    """
+    Output is in the form:
+
+    (
+        [
+            (
+                xTrainSplit_0,
+                xValidationSplit_0,
+                yTrainSplit_0,
+                yValidationSplit_0,
+            ),
+            (
+            ...
+            )
+        ],
+        (
+            xTestSplit,
+            yTestSplit,
+        )
+    )
+    """
+
     absoluteFilepath: Path = fs.convertRelativePathToAbsolute(path=filepath)
+
     df: DataFrame = data.loadDataFromCSV(
         dataFilepath=absoluteFilepath,
         columns=COLUMNS,
     )
-    splits: Tuple[ndarray, ndarray, ndarray, ndarray] = data.prepareClassificationData(
+
+    trainTestSplits: Tuple[
+        ndarray, ndarray, ndarray, ndarray
+    ] = data.prepareClassificationData(
         df=df,
         randomState=randomState,
     )
-    return splits
+
+    trainSplits: Tuple[ndarray, ndarray] = (
+        trainTestSplits[0],
+        trainTestSplits[2],
+    )
+    testSplits: Tuple[ndarray, ndarray] = (
+        trainTestSplits[1],
+        trainTestSplits[3],
+    )
+
+    trainValidationSplits: List[
+        Tuple[ndarray, ndarray, ndarray, ndarray]
+    ] = data.stratifiedKFold(
+        x=trainSplits[0],
+        y=trainSplits[1],
+        splits=splits,
+        randomState=randomState,
+    )
+
+    return (trainValidationSplits, testSplits)
